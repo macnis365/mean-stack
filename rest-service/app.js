5ded613ded949e346f6f702f483cd4cb33e62851
macnis365 <macnis365@gmail.com>
2019-02-17 15:33:56 +0530

    remove gitignore files:

--- rest-service/app.js
+++ rest-service/app.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const db = require('./queries')
+const port = 3000
 
-const app = express();
-const port = 3000;
-
-app.use(bodyParser);
+app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
         extended: true
-    }));
-
-
-/*app.get("/url", (req, res, next) => {
-    res.json(["Tony", "Roger", "Natasha", "Thor", "Hawkeye", "Banner"]);
-});*/
+    })
+)
 
 app.get('/', (request, response) => {
     response.json({
         info: 'Node.js, Express, and Postgres API'
-    });
-});
+    })
+})
 
-var server = app.listen(port, () => {
-    console.log("server running on port 3000");
-});
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
+
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
